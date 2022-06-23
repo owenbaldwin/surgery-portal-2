6858a16a9cb61fda401ca80d09dc38aea6b55ad5
Owen Baldwin <owenabaldwin@gmail.com>
2022-06-23 15:44:49 +0100

    added ranks to migration file along with dob and nin and RANK array to model

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,6 +18,11 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
+  def index
+    @users =User.all
+    index_path
+  end
+
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :omniauthable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, omniauth_providers: [:google_oauth2]
 
+  RANKS = ["admin", "doctor", "nurse", "secretary", "patient"]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
